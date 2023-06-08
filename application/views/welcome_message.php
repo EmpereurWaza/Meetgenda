385956b1a011f74871d4b196d6005c71f9fd4ef7
Jannaire <clement.jannaire@gmail.com>
2023-06-08 15:55:14 +0200

    gg

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,1 +1,25 @@
 <!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <title> Meetgenda </title>
+    <link rel="stylesheet" href="acceuil.css" />
+</head>
+<body>
+    
+
+    <header>
+            <ul>
+                <li><a class="menu__item" href="#">Accueil</a></li>
+                <li><a class="menu__item" href="#">Présentation</a></li>
+                <li><a class="menu__item" href="#">Projet</a></li>
+                <li><a class="menu__item" href="#">Documents</a></li>
+                <li><a class="menu__item" href="inscription.php">Inscription</a></li>
+                <li><a class="menu__item" href="connection.php">Connection</a></li>
+                
+            </ul>
+    </header>
+    
+    </body>
+</html>
