0d95f32b89831a4f2b40324ff802e3d17197f144
Siuda <mateosiuda@aol.com>
2023-06-15 15:55:31 +0200

    Ajout Connexion.php & Inscription.php

--- application/controllers/Connexion.php
+++ application/controllers/Connexion.php
@@ -2,12 +2,29 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Connexion extends CI_Controller {
-	public function index()
-	{
-		$this->load->model('register_model');
-		$this->load->helper('url');
-		$this->load->helper('html');
-		//$this->load->view('header');
-		$this->load->view('connection');
-	}
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        session_start();
+    }
+
+    public function index()
+    {
+        $this->load->view('login/index');
+    }
+
+    public function run()
+    {
+        $this->load->model('Register_model', 'model'); // Charger le modèle Register_model
+        $this->model->run();
+    }
+
+    public function logout()
+    {
+        session_destroy();
+        redirect('connexion/index'); // Utiliser redirect() pour rediriger vers la page de connexion
+        exit;
+    }
 }
--- application/controllers/Inscription.php
+++ application/controllers/Inscription.php
@@ -2,12 +2,40 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Inscription extends CI_Controller {
-	public function index()
-	{
-		$this->load->helper('url');
-		$this->load->helper('html');
-		$this->load->model('register_model');
-		//$this->load->view('header');
-		$this->load->view('inscription');
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('Register_model', 'model'); // Charger le modèle Register_model
+    }
+
+    public function index()
+    {
+        $this->load->view('register/index'); // Utiliser load->view() pour afficher la vue
+    }
+
+    public function signup()
+    {
+        $identifiant = $this->input->post('Identifiant');
+        $email = $this->input->post('Email');
+        $password = $this->input->post('MotDePasse');
+        
+        $count = $this->model->check_user($identifiant, $email);
+        
+        if ($count > 0) {
+            echo 'This User Already Exists';
+        } else {
+            $data = array(
+                'Nom' => null, // Remplacer 'Nom' par le nom de votre colonne correspondante dans la table 'User'
+                'Prenom' => null, // Remplacer 'Prenom' par le nom de votre colonne correspondante dans la table 'User'
+                'Identifiant' => $identifiant,
+                'MotDePasse' => password_hash($password, PASSWORD_DEFAULT),
+                'Email' => $email
+            );
+            
+            $this->model->insert_user($data);
+        }
+        
+        redirect('inscription/index'); // Utiliser redirect() pour rediriger vers la page d'inscription
+    }
 }
