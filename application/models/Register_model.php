f795be3ee83978779fdf6c150f9e9588e29bb1a4
Siuda <mateosiuda@aol.com>
2023-06-15 15:17:46 +0200

    Test v2

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -4,17 +4,19 @@ class Register_model extends CI_Model {
     public function __construct()
     {
         $this->load->database();
-        siuda::init();
+        session_start();
     }
-    public function check_user($identifiant,$email)
-	{
-		$result= $this->db->select("SELECT * FROM `User` WHERE Identifiant = '".$identifiant."' OR Email = '".$email."'");
-		$count = count($result);
-		return $count;
-	}
-	public function insert_user()
-	{
-		$query = 'INSERT INTO User (Nom, Prenom, Identifiant, MotDePasse, Email) VALUES (?, ?, ?, ?, ?)';
+    
+    public function check_user($identifiant, $email)
+    {
+        $result = $this->db->select("SELECT * FROM `User` WHERE Identifiant = '".$identifiant."' OR Email = '".$email."'");
+        $count = count($result);
+        return $count;
+    }
+    
+    public function insert_user()
+    {
+        $query = 'INSERT INTO User (Nom, Prenom, Identifiant, MotDePasse, Email) VALUES (?, ?, ?, ?, ?)';
         $paramType = 'sss';
         $paramValue = array(
             $_POST["Nom"],
@@ -22,38 +24,34 @@ class Register_model extends CI_Model {
             $_POST["Identifiant"],
             $_POST["MotDePasse"],
             $_POST["Email"],
-            );
+        );
         $memberId = $this->ds->insert($query, $paramType, $paramValue);
-        if (! empty($memberId)) {
+        if (!empty($memberId)) {
             $response = array(
                 "status" => "success",
                 "message" => "You have registered successfully."
             );
         }
-	}
+    }
+    
     public function run()
-	{
-		
-		$identifiant=$_POST['Identifiant'];
-		$password=md5($_POST['MotDePasse']);
-		
-		$res= $this->db->select("SELECT * FROM `User` WHERE Identifiant = '".$identifiant."' AND MotDePasse = '".$password."'");
-		$count = count($res);
-		
-		if ($count > 0) {
-			
-			Session::init();
-			Session::set('role', "user");
-			Session::set('loggedIn', true);
-			Session::set('Identifiant', $identifiant);
-			Session::set('MotDePasse', $res[0]['password']);
-			header('location: '.URL.'login/index');
-		} 
-		   else {
-			Session::set('loggedIn', false);
-			header('location: '.URL);
-		}
-		
-		
-	}
+    {
+        $identifiant = $_POST['Identifiant'];
+        $password = md5($_POST['MotDePasse']);
+        
+        $res = $this->db->select("SELECT * FROM `User` WHERE Identifiant = '".$identifiant."' AND MotDePasse = '".$password."'");
+        $count = count($res);
+        
+        if ($count > 0) {
+            session_start();
+            $_SESSION['role'] = "user";
+            $_SESSION['loggedIn'] = true;
+            $_SESSION['Identifiant'] = $identifiant;
+            $_SESSION['MotDePasse'] = $res[0]['password'];
+            header('location: '.URL.'login/index');
+        } else {
+            $_SESSION['loggedIn'] = false;
+            header('location: '.URL);
+        }
+    }
 }
